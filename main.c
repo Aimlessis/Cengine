5536f797c3f303998e609b8c4b07c3c321558e48
Aimlessis <ism.dlsr18@gmail.com>
2025-10-26 00:00:29 -0400

    mouse tracking test

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <stdio.h>
+#include <unistd.h>
+
 
 int main(){
-    printf("hello world :)!!!");
+    Display *display = XOpenDisplay(NULL);
+    Window root = DefaultRootWindow(display);
+    int rootX, rootY, winX, winY;
+    unsigned int mask;
+
+    Window root_disp, child_disp;
+    while(1){
+        if(XQueryPointer(display, root, &root_disp, &child_disp, &rootX, &rootY, &winX, &winY, &mask)){
+            if(mask & Button1Mask){
+                printf("(%4d, %4d) \n", rootX, rootY);
+                break;
+            }
+        }
+    }
+    XCloseDisplay(display);
+
     return 0;
 }
